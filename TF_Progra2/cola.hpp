e6ef22e469b387b2df5347a5de402ae80df181c9
Francesco Bassino <54272736+Frany-oss@users.noreply.github.com>
2019-09-15 23:28:18 -0500

    push

--- TF_Progra2/cola.hpp
+++ TF_Progra2/cola.hpp
@@ -4,21 +4,39 @@ template <typename C>
 class cola {
 
 	struct Node {
-
 		Node* next;
 		C elem;
 
-		Node(C elem, Node* next = nullptr) 
-			: elem(elem), next(next){ }
+		Node(C elem) : next(next) { }
 	};
 
 	Node* head;
 	Node* tail;
 
 public:
-	cola() { }
 
-	C peek() {
-		return head->elem;
+	bool is_Empty() {
+		return head == nullptr;
 	}
+
+	void add(C elem) {
+		Node* node = new Node(elem);
+		if (tail != nullptr) {
+			tail->next = node;
+		}
+		tail = node;
+		if (head == nullptr) {
+			head = node;
+		}
+	}
+
+	int remove() {
+		C elem = head->elem;
+		head = head->next;
+		if (head == nullptr) {
+			tail = nullptr;
+		}
+		return elem;
+	}
+
 };
